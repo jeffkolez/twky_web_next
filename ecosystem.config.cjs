368ed8d6962924ed32c46bb65c5bad709b559405
jeffkolez <jeffkolez@gmail.com>
2025-09-19 15:21:06 -0700

    ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'twky-web',
-            cwd: '/home/forge/test.theywillkillyou.com',
+            cwd: '/home/forge/theywillkillyou.com',
             script: 'node_modules/next/dist/bin/next',
             args: 'start -p 3000',
             env: { NODE_ENV: 'production' }
