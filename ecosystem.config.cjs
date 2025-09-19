3c7b0b996ac943e149d0c3316aabc8e8a57cc0d4
jeffkolez <jeffkolez@gmail.com>
2025-09-19 15:38:57 -0700

    test setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,18 @@
 module.exports = {
     apps: [
-        {
-            name: 'twky-web',
-            cwd: '/home/forge/theywillkillyou.com',
-            script: 'node_modules/next/dist/bin/next',
-            args: 'start -p 3000',
-            env: { NODE_ENV: 'production' }
-        }
+            {
+                name: 'twky-web',
+                cwd: '/home/forge/theywillkillyou.com',
+                script: 'node_modules/.bin/next',
+                args: 'start -p 3000',
+                env: { NODE_ENV: 'production' }
+            },
+            {
+                name: 'twky-web-test',
+                cwd: '/home/forge/test.theywillkillyou.com',
+                script: 'node_modules/.bin/next',
+                args: 'start -p 3001',
+                env: { NODE_ENV: 'production' }
+            }
     ]
 };
