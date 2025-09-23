0ae962a4dd1065b5f833e88f34cb02c9ae082001
jeffkolez <jeffkolez@gmail.com>
2025-09-23 14:09:52 -0700

    ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <Script
+                    id="ezoic-sa"
+                    src="//www.ezojs.com/ezoic/sa.min.js"
+                    strategy="afterInteractive"
+                    async
+                />
+                <Script id="ezoic-init" strategy="beforeInteractive">
+                    {`
+                    window.ezstandalone = window.ezstandalone || {};
+                    ezstandalone.cmd = ezstandalone.cmd || [];
+                    `}
+                </Script>
             </head>
             <body>
                 <MantineProvider defaultColorScheme="light">
